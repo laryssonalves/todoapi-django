8424784b4211f51f14660b442f0507821fc5937c
larysson <larysson.alves@gmail.com>
2020-08-31 22:59:14 -0300

    debug false

--- todolist/settings.py
+++ todolist/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'v7sqr$n5%6sd8+t9u#j^glocdohguti8i^epm-a*yfcuz-i-an'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
