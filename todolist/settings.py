bb3393b1a3dc9ee7395a2015053b83282c7aad26
larysson <larysson.alves@gmail.com>
2020-08-31 23:48:24 -0300

    trying to fix deploy error

--- todolist/settings.py
+++ todolist/settings.py
@@ -132,6 +132,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d/%m/%y %H:%M',
