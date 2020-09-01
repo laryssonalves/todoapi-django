3fac24eba96ca45995d593fbf9e5919199a9adc5
larysson <larysson.alves@gmail.com>
2020-08-31 23:53:23 -0300

    trying to fix deploy error

--- todolist/settings.py
+++ todolist/settings.py
@@ -79,23 +79,23 @@ WSGI_APPLICATION = 'todolist.wsgi.application'
 CORS_ALLOW_ALL_ORIGINS = True
 ALLOWED_HOSTS = ['localhost', ".herokuapp.com"]
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        }
-    }
-else:
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#         }
+#     }
+# else:
     # CORS_ALLOWED_ORIGINS = [
     #     "https://todoweb-react.herokuapp.com/",
     # ]
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -132,7 +132,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 REST_FRAMEWORK = {
     'DATETIME_FORMAT': '%d/%m/%y %H:%M',
